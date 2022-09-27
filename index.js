5279147ec832871d3815fd9cd8c0ee05786a26f2
rokmare33 <mirko.sgambellone@gmail.com>
2022-09-27 23:45:40 +0200

    setup rendering html pages

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = new express();
 
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+
+app.use(express.static("public"));
+app.use("/css", express.static(`${__dirname}/public/css`));
+
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.sendFile(__dirname + "/index.html");
 });
 
 const PORT = process.env.PORT || 9000;
